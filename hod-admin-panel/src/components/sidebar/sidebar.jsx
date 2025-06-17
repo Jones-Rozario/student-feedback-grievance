9d34bc7954310b62d17553a8637fd92b228a3687
Jones Rozario <jonesrozariojj@gmail.com>
2025-06-17 22:08:20 +0530

    Updated profile and gap in sidebar

--- hod-admin-panel/src/components/sidebar/sidebar.jsx
+++ hod-admin-panel/src/components/sidebar/sidebar.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import { FaHome, FaUsers, FaComments } from 'react-icons/fa';
-import './sidebar.scss';
+import React from "react";
+import { FaHome, FaUsers, FaComments } from "react-icons/fa";
+import "./sidebar.scss";
 
 const Sidebar = ({ open }) => (
-  <div className={`sidebar ${open ? 'open' : 'closed'}`}>
+  <div className={`sidebar ${open ? "open" : "closed"}`}>
     <div className="sidebar__profile">
-      <img src="/profile.jpg" alt="Admin" />
-      <div>
+      <img src="/assets/hod-profile.jpeg" alt="Admin" />
+      <div className="sidebar__profile-info">
         <div className="sidebar__name">Admin</div>
         <div className="sidebar__role">HOD</div>
       </div>
     </div>
     <nav>
-      <a href="/" className="sidebar__link"><FaHome className="sidebar__icon" /> Dashboard</a>
-      <a href="/faculties" className="sidebar__link"><FaUsers className="sidebar__icon" /> Faculties</a>
-      <a href="/grievances" className="sidebar__link"><FaComments className="sidebar__icon" /> Grievances</a>
+      <a href="/" className="sidebar__link">
+        <FaHome className="sidebar__icon" /> Dashboard
+      </a>
+      <a href="/faculties" className="sidebar__link">
+        <FaUsers className="sidebar__icon" /> Faculties
+      </a>
+      <a href="/grievances" className="sidebar__link">
+        <FaComments className="sidebar__icon" /> Grievances
+      </a>
     </nav>
   </div>
 );
